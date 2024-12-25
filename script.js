c8b181b8018186a432e3d71a00ae2034788a5b5d
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-25 19:40:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const text = "We love Programming!";
+
+const para = document.querySelector("#para");
+
+let input = prompt("Enter input fom 1 to 10.");
+input = parseInt(input);
+
+let i=0;
+const interval = setInterval(() => {
+	if(i>=text.length) {
+		clearInterval(interval);
+		return;
+	}
+	console.log(i);
+	para.innerText += text[i];
+	i++;
+}, 500/input);
+
+
+
+
+
