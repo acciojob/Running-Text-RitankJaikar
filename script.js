ecd193e9cef06eff13035d18d4ccbcd331eb6190
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-27 13:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
+const textEl = document.getElementById("text");
+const speedEl = document.getElementById("speed");
+ 
 const text = "We love Programming!";
-const para = document.querySelector("#text");
-const speedInput = document.querySelector("#speed");
-
-let interval;
-let i = 0;
-
-const displayText = () => {
-  if (interval) clearInterval(interval);
-
-  const speed = parseInt(speedInput.value);
-
-  if (!speed || speed < 1 || speed > 10) {
-    alert("Please enter a speed between 1 and 10.");
-    return;
+ 
+let idx = 1;
+ 
+const NUM = 500;
+let time = NUM / speedEl.value;
+ 
+writeText();
+ 
+function writeText() {
+  textEl.innerHTML = text.slice(0, idx);
+ 
+  idx++;
+ 
+  if (idx > text.length) {
+    idx = 1;
   }
-
-  para.innerHTML = "";
-  i = 0;
-
-  interval = setInterval(() => {
-    if (i >= text.length) {
-      clearInterval(interval);
-      return;
-    }
-    para.innerHTML += text[i];
-    i++;
-  }, 500 / speed);
-};
-
-speedInput.addEventListener("input", displayText);
+ 
+  setTimeout(writeText, time);
+}
+ 
+speedEl.addEventListener("input", (e) => {
+  time = NUM / e.target.value;
+});
